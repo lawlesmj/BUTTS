b51546c0f22d9166155fdefaeb63c0b11dd81b14
lawlesmj <lawlesmj@dukes.jmu.edu>
2013-09-17 15:13:43 -0400

    Update toyUser.c

--- toys/toyUser.c
+++ toys/toyUser.c
@@ -19,20 +19,35 @@ int main(){
     int shmid;
     key_t shmkey;
     int shmflag;
+    toy thetoy;
     
     my_pid = getpid();
     shmkey = TOYBOX_KEY;
     shmflag = IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR ;
     
+    printf("\nOnce upon a time...");
+    
+    //create memory
     shmid = shmget( shmkey, BOX_SIZE, shmflag);
     
     if ( shmid != -1 ) {
-        printf("\nThe Parent with id %d  bought a toybox labeled \"%d\"\n"
+        printf("\nThe Parent with id %d bought a toybox labeled \"%d\""
              , my_pid , shmid) ;
     }
     else {
-        printf("\nFailed to access shared memory \"0x%X\". errno=%d\n"
-             , shmkey , errno ) ;
+        printf("\nThe Parent with id %d failed to buy a toybox. \"Errno %d\", the parent swears."
+             , my_pid , errno ) ;
         exit(-1) ;    
     }
+    
+    //attach to memory
+    p = (toybox *) shmat( shmid, NULL, 0);
+    if ( p == (toybox *) -1) {
+        printf("\nThe parent could not open the toybox. The parent gives up and terminates himself. The end.");
+        exit(-1);
+    }
+    
+    thetoy->name = "Bear";
+    p->toys[0] 
+    printf("\nThe parent creates a toy %s. The parent puts the toy in the box.", thetoy->name);
 }
