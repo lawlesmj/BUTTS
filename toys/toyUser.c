6a2a1606e6e5aba844c0deff99de2fd5d693d6b5
applegde <applegde@dukes.jmu.edu>
2013-09-17 14:27:54 -0400

    Update and rename user.c to toyUser.c

---
+++ toys/toyUser.c
@@ -0,0 +1,17 @@
+/**************************************************************************
+ * Authors: Douglas Applegate and Mike Lawless
+ * Assignment Title: IPC demo
+ * **************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include "toybox.h" 
+
+int main(){
+    
+}
