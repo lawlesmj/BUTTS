4148c5e07ead444cb64cef8faeba48b1fc31953c
lawlesmj <lawlesmj@dukes.jmu.edu>
2013-09-17 13:53:57 -0400

    Create user.c

---
+++ passing_notes/user.c
@@ -0,0 +1,9 @@
+//some includes
+#include "passing_notes.h"
+
+/*
+  user processes send notes to mailbox, labeled with their process id (pid)
+  
+  able to check mailbox, then continue
+  able to send mail to mailbox (with specific reciever?)
+*/
