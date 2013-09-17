4bb8fb2dda91ebc549e149686797b5f5c4761d1b
applegde <applegde@dukes.jmu.edu>
2013-09-17 14:57:54 -0400

    Update toyUser.c

--- toys/toyUser.c
+++ toys/toyUser.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -15,7 +16,23 @@
 
 int main(){
     pid_t my_pid, pid;
+    int shmid;
+    key_t shmkey;
+    int shmflag;
     
     my_pid = getpid();
+    shmkey = TOYBOX_KEY;
+    shmflag = IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR ;
     
+    shmid = shmget( shmkey, BOX_SIZE, shmflag);
+    
+    if ( shmid != -1 ) {
+        printf("\nThe Parent with id %d  bought a toybox labeled \"%d\"\n"
+             , my_pid , shmid) ;
+    }
+    else {
+        printf("\nFailed to access shared memory \"0x%X\". errno=%d\n"
+             , shmkey , errno ) ;
+        exit(-1) ;    
+    }
 }
