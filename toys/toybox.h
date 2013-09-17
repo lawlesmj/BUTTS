7db8e46dbf062314077577740bd87ab39adc29ac
lawlesmj <lawlesmj@dukes.jmu.edu>
2013-09-17 14:34:13 -0400

    Update toybox.h

--- toys/toybox.h
+++ toys/toybox.h
@@ -1,14 +1,15 @@
-// some includes
-
 #ifndef TOYBOX_H
 #define TOYBOX_H
 
-//constants:
-//  shared memory key
+// shared memory key
 #define TOYBOX_KEY 0x666
 
-//structure:
-//  toy structure
-//  toybox structure
+typedef struct {
+  
+} toy;
+
+typedef struct {
+  
+} toybox;
 
 #endif
