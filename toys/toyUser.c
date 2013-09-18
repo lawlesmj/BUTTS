e3db6a86558d208b910dc9b9beb3626590ffb61d
applegde <applegde@dukes.jmu.edu>
2013-09-18 17:03:11 -0400

    Update toyUser.c

--- toys/toyUser.c
+++ toys/toyUser.c
@@ -7,10 +7,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <unistd.h>
+#include <sys/stat.h>
+
 
 #include "toybox.h" 
 
