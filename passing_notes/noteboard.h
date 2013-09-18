b4744f01e10760cb4813c3c1a2a9fb909e72b0da
applegde <applegde@dukes.jmu.edu>
2013-09-18 18:37:15 -0400

    Update noteboard.h

--- passing_notes/noteboard.h
+++ passing_notes/noteboard.h
@@ -1,12 +1,16 @@
-// some includes
+#include <sys/types.h>
 
 #ifndef PASSING_NOTES_H
 #define PASSING_NOTES_H
 
-//constants:
-//  mailbox
+#define MSGQUE_KEY 0xF00D
 
-//structure:
-//  message structure
+typedef struct {
+  long mtype;
+  struct {
+    pid_t sender;
+    char text[128];
+  } note;
+} msgbuf;
 
 #endif
