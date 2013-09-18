8ce700edc257c43b2d9cbf2e7981582b8254c028
applegde <applegde@dukes.jmu.edu>
2013-09-18 19:02:43 -0400

    Update user.c

--- passing_notes/user.c
+++ passing_notes/user.c
@@ -9,9 +9,32 @@
 
 #include "noteboard.h"
 
-/*
-  user processes send notes to mailbox, labeled with their process id (pid)
+int main() {
+  msgbuf buf;
+  int msgqid;
+  key_t key;
   
-  able to check mailbox, then continue
-  able to send mail to mailbox (with specific reciever?)
-*/
+  key = MSGQUE_KEY;
+  
+  if((msqid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+    perror("msgget");
+    exit(1);
+  }
+  
+  printf("Enter lines of text, ^D to quit:\n");
+  buf.mtype = 1; 
+  while(fgets(buf.note.text, sizeof buf.note.text, stdin) != NULL) {
+    int len = strlen(buf.note.text);
+    
+    if (buf.note.text[len-1] == '\n') buf.note.text[len-1] = '\0';
+    if (msgsnd(msqid, &buf, len+1, 0) == -1) 
+    perror("msgsnd");
+  }
+  
+  // destroy the box
+  if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+    perror("msgctl");
+    exit(1);
+  }
+  return 0;
+}
